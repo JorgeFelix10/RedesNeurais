f3af1bd56a59186e7972c10c74a719b382af3bb4
JorgeFelix10 <119880007+JorgeFelix10@users.noreply.github.com>
2024-02-28 21:23:12 -0300

    commit 28/02/2024 parte 2

--- MultiLayerPerceptron/multi_layer_perceptron.py
+++ MultiLayerPerceptron/multi_layer_perceptron.py
@@ -11,33 +11,48 @@ def sigmoidderivada(sig):
 
 entradas = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 saidas = np.array([[0], [1], [1], [0]])
+"""
 pesos0 = np.array([[-0.424, -0.740, -0.961],
                    [0.358, -0.577, 0.469]])
 pesos1 = np.array([[-0.017], [-0.893], [0.148]])
+"""
 
-epocas = 100
+pesos0 = 2 * np.random.random((2, 3)) - 1
+pesos1 = 2 * np.random.random((3, 1)) - 1
+epocas = 100000
 taxaAprendizagem = 0.3
 momento = 1
+# ESSE TRECHO DE CÓDIGO SERVE PARA PREVINIR A WARNING CAN BE UNDEFINED
+camadaSaida = np.array([0, 1, 1, 0])
 
 # FAZER UM FOR COM O NÚMERO DE EPOCAS
+for _ in range(epocas):
+    camadaEntrada = entradas
+    somaSinapse0 = np.dot(camadaEntrada, pesos0)
+    camadaOculta = sigmoid(somaSinapse0)
 
-camadaEntrada = entradas
-somaSinapse0 = np.dot(camadaEntrada, pesos0)
-camadaOculta = sigmoid(somaSinapse0)
+    somaSinapse1 = np.dot(camadaOculta, pesos1)
+    camadaSaida = sigmoid(somaSinapse1)
 
-somaSinapse1 = np.dot(camadaOculta, pesos1)
-camadaSaida = sigmoid(somaSinapse1)
+    erroCamadaSaida = saidas - camadaSaida
+    mediaAbsoluta = np.mean(np.abs(erroCamadaSaida))
+    print(f'Erro:{str(mediaAbsoluta)}')
 
-erroCamadaSaida = saidas - camadaSaida
-mediaAbsoluta = np.mean(np.abs(erroCamadaSaida))
+    derivadaSaida = sigmoidderivada(camadaSaida)
+    deltaSaida = erroCamadaSaida * derivadaSaida
 
-derivadaSaida = sigmoidderivada(camadaSaida)
-deltaSaida = erroCamadaSaida * derivadaSaida
+    pesos1Transposta = pesos1.T
+    deltaSaidaXPesos = deltaSaida.dot(pesos1Transposta)
+    deltaCamadaOculta = deltaSaidaXPesos * sigmoidderivada(camadaOculta)
 
-pesos1Transposta = pesos1.T
-deltaSaidaXPesos = deltaSaida.dot(pesos1Transposta)
-deltaCamadaOculta = deltaSaidaXPesos * sigmoidderivada(camadaOculta)
+    camadaOcultaTransposta = camadaOculta.T
+    pesosNovos1 = camadaOcultaTransposta.dot(deltaSaida)
+    pesos1 = (pesos1 * momento) + (pesosNovos1 * taxaAprendizagem)
 
-camadaOcultaTransposta = camadaOculta.T
-pesosNovos1 = camadaOcultaTransposta.dot(deltaSaida)
-pesos1 = (pesos1 * momento) + (pesosNovos1 * taxaAprendizagem)
+    camadaEntradaTransposta = camadaEntrada.T
+    pesosNovos0 = camadaEntradaTransposta.dot(deltaCamadaOculta)
+    pesos0 = (pesos0 * momento) + (pesosNovos0 * taxaAprendizagem)
+
+# print(camadaSaida)
+print(f' \n {round(camadaSaida[0][0])} \n {round(camadaSaida[1][0])} \n {round(camadaSaida[2][0])}'
+      f' \n {round(camadaSaida[3][0])}')
